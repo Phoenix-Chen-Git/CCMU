b97227c9e9050ec719f37b38b37cff7c5d24b549
phoenix <115352360+Phoenix-Chen-Git@users.noreply.github.com>
2025-05-07 13:37:35 +0000

    added scripts

---
+++ download.sh
@@ -0,0 +1,7 @@
+#download epi_data
+cd epidata/
+wget https://publicphoenixchen.s3.us-east-1.amazonaws.com/epi_genetic/selected_tracks.tar.gz
+tar -xzf selected_tracks.tar.gz
+cd ..
+#download weight
+wget https://publicphoenixchen.s3.us-east-1.amazonaws.com/weights/
