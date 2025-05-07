fa7fab39cf4fc324c120c9c58e2063708fcd5316
phoenix <115352360+Phoenix-Chen-Git@users.noreply.github.com>
2025-05-07 13:54:39 +0000

    adjust download.sh

--- download.sh
+++ download.sh
@@ -1,7 +1,14 @@
 #download epi_data
+mkdir epi_data/
 cd epidata/
 wget https://publicphoenixchen.s3.us-east-1.amazonaws.com/epi_genetic/selected_tracks.tar.gz
 tar -xzf selected_tracks.tar.gz
 cd ..
 #download weight
-wget https://publicphoenixchen.s3.us-east-1.amazonaws.com/weights/
+mkdir weights/
+cd weights/
+wget https://publicphoenixchen.s3.us-east-1.amazonaws.com/weights/best_model_1th.pth
+wget https://publicphoenixchen.s3.us-east-1.amazonaws.com/weights/best_model_2th.pth
+wget https://publicphoenixchen.s3.us-east-1.amazonaws.com/weights/best_model_3th.pth
+wget https://publicphoenixchen.s3.us-east-1.amazonaws.com/weights/best_model_4th.pth
+wget https://publicphoenixchen.s3.us-east-1.amazonaws.com/weights/best_model_5th.pth
