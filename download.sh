e58484a5dd9cfccbc577ab8e5fb77e9748c4a6e8
phoenix <115352360+Phoenix-Chen-Git@users.noreply.github.com>
2025-05-07 14:08:25 +0000

    updated

--- download.sh
+++ download.sh
@@ -1,14 +1,13 @@
 #download epi_data
 mkdir epi_data/
-cd epidata/
 wget https://publicphoenixchen.s3.us-east-1.amazonaws.com/epi_genetic/selected_tracks.tar.gz
 tar -xzf selected_tracks.tar.gz
-cd ..
+mv *.bigWig epi_data/
 #download weight
 mkdir weights/
-cd weights/
 wget https://publicphoenixchen.s3.us-east-1.amazonaws.com/weights/best_model_1th.pth
 wget https://publicphoenixchen.s3.us-east-1.amazonaws.com/weights/best_model_2th.pth
 wget https://publicphoenixchen.s3.us-east-1.amazonaws.com/weights/best_model_3th.pth
 wget https://publicphoenixchen.s3.us-east-1.amazonaws.com/weights/best_model_4th.pth
 wget https://publicphoenixchen.s3.us-east-1.amazonaws.com/weights/best_model_5th.pth
+mv *.pth weights/
