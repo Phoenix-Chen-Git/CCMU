2c9173464b5d49450ce56ee5ba35f700227a3cb6
phoenix <115352360+Phoenix-Chen-Git@users.noreply.github.com>
2025-06-23 07:46:11 +0000

    cleaned the scripts.

--- download_scripts/download_epi_data.sh
+++ download_scripts/download_epi_data.sh
@@ -1,4 +1,4 @@
 cd ../
 mkdir epi_data
 cd epi_data
-wget https://zenodo.org/records/15715343/files/selected_tracks.tar?download=1
+wget https://zenodo.org/records/15715343/files/selected_tracks.tar
